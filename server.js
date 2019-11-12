80bd0af3ffe48d381f88e6ce063fc5a071a3934e
Alvaro Romero <Alvaro.Romero.Ayala@everis.com>
2019-11-12 10:00:42 +0100

    change name app

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/polar-stream-36841'));
+app.use(express.static(__dirname + '/dist/cv-project'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/polar-stream-36841/index.html'));
+res.sendFile(path.join(__dirname + '/dist/cv-project/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
